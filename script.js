a2c0195d435d406e519f6ba0e1388f8651b58460
PrabinGautam <prabeen122@gmail.com>
2022-07-19 22:48:14 +0545

    Refactoring

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
 let textarea = document.querySelector("#textarea-container");
-let prediction = document.querySelector("#predict");
 
-const suggestionWord = ["Prabin Gautam", "Prabin Gautam", "Gaming", "meet you"];
+const suggestionWord = [
+  "Prabin Gautam",
+  "Prabin Gautam",
+  "Gaming",
+  "meet you",
+  "one",
+];
 
 const suggestionActivation = [
   "My name is",
   "My name is Prabin Gautam. My name is",
   "My name is Prabin Gautam. I love",
   "Nice to",
+  "Two are better than",
 ];
 
 let tabKeyPressed = false;
 let showSuggestions = false;
 let suggestedWord = "";
+let hasPredictContainer = false;
+
+function setCursor() {
+  const selection = window.getSelection();
+  const range = document.createRange();
+  selection.removeAllRanges();
+  range.selectNodeContents(textarea);
+  range.collapse(false);
+  selection.addRange(range);
+  textarea.focus();
+}
+
+function addSuggestions(event, suggestedWord) {
+  let textContents = event.target.childNodes[0].textContent;
+  event.target.childNodes[0].textContent = `${textContents} ${suggestedWord}`;
+  textarea.focus();
+}
+
+const getSuggestedWord = function (userText) {
+  let index = suggestionActivation.findIndex((value) => value == userText);
+  return suggestionWord[index];
+};
+
+function showSuggestionWord(suggestedWord) {
+  temp_element = `<span id="predict" class="predict" contenteditable="false">
+          <span class="predict-word">${suggestedWord}</span>
+          <span class="predict-word tab">TAB</span>
+        </span>`;
+  textarea.insertAdjacentHTML("beforeend", temp_element);
+  hasPredictContainer = true;
+}
+
+const resetSuggestions = function () {
+  suggestedWord = "";
+  showSuggestions = false;
+  hasPredictContainer = false;
+};
 
 document.addEventListener("keydown", (event) => {
   tabKeyPressed = event.key === "Tab";
@@ -25,66 +68,31 @@ document.addEventListener("keydown", (event) => {
 
 document.addEventListener("keyup", predict);
 
-function setCursor() {
-  const selection = window.getSelection();
-  const range = document.createRange();
-  selection.removeAllRanges();
-  range.selectNodeContents(textarea);
-  range.collapse(false);
-  selection.addRange(range);
-  textarea.focus();
-}
-
-function addSuggedtedWord(event, suggestedWord) {
-  let textContents = event.target.childNodes[0].textContent;
-
-  event.target.childNodes[0].textContent = `${textContents} ${suggestedWord}`;
-  textarea.focus();
-}
-
-const findSuggestionWord = function (userText) {
-  let index = suggestionActivation.findIndex((value) => value == userText);
-  return suggestionWord[index];
-};
-
 function predict(event) {
   const data = event.target.textContent
     ? event.target.childNodes[0].textContent
     : "";
 
   showSuggestions = suggestionActivation.some((value) => data == value);
-  suggestedWord = findSuggestionWord(data);
 
-  const contains = document.body.contains(document.getElementById("predict"));
-  const resetSuggestions = function () {
-    suggestedWord = "";
-    showSuggestions = false;
-  };
+  let predictContainer = document.getElementById("predict");
+  hasPredictContainer = Boolean(predictContainer);
 
-  if (showSuggestions) {
-    temp_element = `<span id="predict" class="predict" contenteditable="false">
-          <span class="predict-word">${suggestedWord}</span>
-          <span class="predict-word tab">TAB</span>
-        </span>`;
-
-    if (!contains) {
-      textarea.insertAdjacentHTML("beforeend", temp_element);
-    }
-  } else {
-    if (contains) {
-      document.getElementById("predict").remove();
-      resetSuggestions();
-    }
+  if (showSuggestions && !hasPredictContainer) {
+    //Implement ML model here
+    suggestedWord = getSuggestedWord(data);
+    showSuggestionWord(suggestedWord);
   }
 
-  if (tabKeyPressed) {
+  if (!showSuggestions && hasPredictContainer) {
+    predictContainer.remove();
+    resetSuggestions();
+  }
+
+  if (tabKeyPressed && hasPredictContainer) {
     setCursor();
-    if (contains) {
-      document.getElementById("predict").remove();
-    }
-    if (suggestedWord) {
-      addSuggedtedWord(event, suggestedWord);
-      resetSuggestions();
-    }
+    predictContainer.remove();
+    addSuggestions(event, suggestedWord);
+    resetSuggestions();
   }
 }
